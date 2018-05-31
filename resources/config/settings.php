e7651761ab6d23357faa414b4f00d08a3bfbeaa3
William <williamastrom@gmail.com>
2018-05-31 23:36:17 +0200

    Working on other color schemes

--- resources/config/settings.php
+++ resources/config/settings.php
@@ -5,4 +5,15 @@ return [
         'type'       => 'anomaly.field_type.textarea',
         'input_view' => 'pixney.theme.hjortron::admin/navigation/settings',
     ],
+    'color_scheme'        => [
+        'type'   => 'anomaly.field_type.select',
+        'config' => [
+            'options'       => [
+                'Dark'   => 'Dark',
+                'Blue'   => 'Blue'
+            ],
+            'default_value' => 'dark',
+            'mode'          => 'dropdown',
+        ],
+    ],
 ];
